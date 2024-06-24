ff571de9ab6988e077a2c3a5e8900ef53b47ea7f
hdsjiw <105874382+hdsjiw@users.noreply.github.com>
2024-06-24 13:58:40 +0900

    [Silver IV] Title: 스택 2, Time: 476 ms, Memory: 209576 KB -BaekjoonHub

---
+++ 백준/Silver/28278. 스택 2/스택 2.py
@@ -0,0 +1,28 @@
+import sys
+n=int(sys.stdin.readline())
+stack=[]
+
+for i in range(n):
+    a=sys.stdin.readline().split()
+    if a[0]=='1':
+        stack.append(a[1])
+    elif a[0]=='2':
+        if stack:
+            print(stack.pop())
+        else:
+            print(-1)
+    elif a[0]=='3':
+        print(len(stack))
+    elif a[0]=='4':
+        if stack:
+            print(0)
+        else:
+            print(1)
+    else: #a[0]=='5'
+        if stack:
+            print(stack[-1])
+        else:
+            print(-1)
+        
+
+        
