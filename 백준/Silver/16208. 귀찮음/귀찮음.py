7c847ae45efa3edd7bfab5da42626d8fb174e0a7
hdsjiw <105874382+hdsjiw@users.noreply.github.com>
2025-03-14 11:16:02 +0900

    [Silver V] Title: 귀찮음, Time: 176 ms, Memory: 173760 KB, Score: 30 point -BaekjoonHub

--- 백준/Silver/16208. 귀찮음/귀찮음.py
+++ 백준/Silver/16208. 귀찮음/귀찮음.py
@@ -1,12 +1,11 @@
 n=int(input())
 length=list(map(int,input().split()))
+
 sum_len=sum(length)
 cost=0
 
-for i in range(n-1):
-    x=min(length)
-    length.remove(x)
-    sum_len-=x
-    cost+=sum_len*x
+for i in range(len(length)-1):
+    sum_len-=length[i]
+    cost+=sum_len*length[i]
 
 print(cost)
