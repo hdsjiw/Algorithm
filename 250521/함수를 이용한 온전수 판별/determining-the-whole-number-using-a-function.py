e88d3598f827b08f7ebbfcb0fa2e1efa1a66447d
hdsjiw <105874382+hdsjiw@users.noreply.github.com>
2025-05-21 12:53:56 +0900

    Title: 함수를 이용한 온전수 판별, Time: 44ms, Memory: 16MB, Status: Passed - Codetree

--- 250521/함수를 이용한 온전수 판별/determining-the-whole-number-using-a-function.py
+++ 250521/함수를 이용한 온전수 판별/determining-the-whole-number-using-a-function.py
@@ -6,8 +6,10 @@ num=0
 def perfact_num(n):
     return (n%2==0) or (n%10==5) or ((n%3==0) and (n%9==0))
 
+def perfact_num2(n):
+    return (n%2!=0) and (n%10!=5) and ((n%3!=0) or (n%9==0))
 
 for i in range(a,b+1):
-    if not perfact_num(i):
+    if perfact_num2(i):
         num+=1
 print(num)
