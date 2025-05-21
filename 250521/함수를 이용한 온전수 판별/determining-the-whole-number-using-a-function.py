d8775cbad60b0d10ade3f71f7180bc53e3abf56a
hdsjiw <105874382+hdsjiw@users.noreply.github.com>
2025-05-21 12:51:42 +0900

    Title: 함수를 이용한 온전수 판별, Time: 42ms, Memory: 16MB, Status: Wrong Answer - Codetree

--- 250521/함수를 이용한 온전수 판별/determining-the-whole-number-using-a-function.py
+++ 250521/함수를 이용한 온전수 판별/determining-the-whole-number-using-a-function.py
@@ -2,19 +2,15 @@ a, b = map(int, input().split())
 
 # Please write your code here.
 num=0
-
-def discrimination(n):
-    if perfact_num(n):
-        return False
-    else:
-        return True
-        
+    
 def perfact_num(n):
     return (n%2==0) or (n%10==5) or ((n%3==0) and (n%9==0))
 
 
 for i in range(a,b+1):
-    if discrimination(i):
+    if perfact_num(i):
+        # 아무 명령어
+        i=i
+    else:
         num+=1
-
 print(num)
