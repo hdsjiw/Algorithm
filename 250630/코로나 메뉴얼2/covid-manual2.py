ca6dd8f1c2eac8c13a358526eae2b8f158c9dddb
hdsjiw <105874382+hdsjiw@users.noreply.github.com>
2025-06-30 23:27:43 +0900

    Title: 코로나 메뉴얼2, Time: 48ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 250630/코로나 메뉴얼2/covid-manual2.py
@@ -0,0 +1,19 @@
+med=[0]*4
+for i in range(3):
+    state,temp=input().split()
+    temp=int(temp)
+
+    if state=="Y" and temp>=37:
+        med[0]+=1
+    elif state=="N" and temp>=37:
+        med[1]+=1
+    elif state=='"Y' and temp<37:
+        med[2]+=1
+    else:
+        med[3]+=1
+
+if med[0]>=2:
+    med.append("E")
+
+for elm in med:
+    print(elm,end=" ")
