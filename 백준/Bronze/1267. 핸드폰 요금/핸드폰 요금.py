e35076881fb10c666b47745f434f1abf075498a0
hdsjiw <105874382+hdsjiw@users.noreply.github.com>
2025-11-03 00:21:38 +0900

    [Bronze III] Title: 핸드폰 요금, Time: 88 ms, Memory: 108384 KB -BaekjoonHub

---
+++ 백준/Bronze/1267. 핸드폰 요금/핸드폰 요금.py
@@ -0,0 +1,16 @@
+import sys
+
+n = int(input())
+time = list(map(int, input().split()))
+y = m = 0
+for n in time:
+    y += (n//30 + 1) * 10
+    m += (n//60 + 1) * 15
+if m == y:
+    print("Y M", m)
+elif m < y:
+    print("M", m)
+else:
+    print("Y", y)
+   
+    
