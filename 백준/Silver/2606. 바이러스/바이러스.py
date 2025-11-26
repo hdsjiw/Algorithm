1ad9b56ec11fff27f4a5940fec8ef2f1645ca8c3
hdsjiw <105874382+hdsjiw@users.noreply.github.com>
2025-11-26 18:27:43 +0900

    [Silver III] Title: 바이러스, Time: 88 ms, Memory: 108384 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.py
+++ 백준/Silver/2606. 바이러스/바이러스.py
@@ -1,4 +1,3 @@
-from collections import deque
 n=int(input()) # 컴퓨터 개수
 v=int(input()) # 연결선 개수
 graph = [[] for i in range(n+1)] # 그래프 초기화
@@ -7,12 +6,10 @@ for i in range(v): # 그래프 생성
     a,b=map(int,input().split())
     graph[a]+=[b] # a에 b 연결
     graph[b]+=[a] # b에 a 연결 -> 양방향
-visited[1]=1 # 1번 컴퓨터부터 시작이니 방문 표시
-Q=deque([1])
-while Q:
-    c=Q.popleft()
-    for nx in graph[c]:
+def dfs(v):
+    visited[v]=1
+    for nx in graph[v]:
         if visited[nx]==0:
-            Q.append(nx)
-            visited[nx]=1
+            dfs(nx)
+dfs(1)
 print(sum(visited)-1)
