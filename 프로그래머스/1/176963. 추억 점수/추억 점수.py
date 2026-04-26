ecc0ad4d121037e663c5fccf3b3fd00a2dc549fd
hdsjiw <105874382+hdsjiw@users.noreply.github.com>
2026-04-26 13:01:33 +0900

    [level 1] Title: 추억 점수, Time: 1.14 ms, Memory: 9.93 MB -BaekjoonHub

--- 프로그래머스/1/176963. 추억 점수/추억 점수.py
+++ 프로그래머스/1/176963. 추억 점수/추억 점수.py
@@ -1,10 +1,11 @@
 def solution(name, yearning, photo):
-    answer=[]
-    for i in range(len(photo)):
-        score=0
-        for j in range(len(photo[i])):
-            if photo[i][j] in name: # 그리워하는 사람일 때
-                idx=name.index(photo[i][j])
-                score+=yearning[idx]
+    score_map = dict(zip(name, yearning))  # 이름 → 점수 매핑
+    answer = []
+
+    for people in photo:
+        score = 0
+        for person in people:
+            score += score_map.get(person, 0)  # 없으면 0
         answer.append(score)
+
     return answer
