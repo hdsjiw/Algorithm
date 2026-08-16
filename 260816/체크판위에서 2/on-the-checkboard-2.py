866f38ee3aa6f6587c8d8dacd2ce67240f4eee63
hdsjiw <105874382+hdsjiw@users.noreply.github.com>
2026-08-16 22:25:54 +0900

    [Passed] 체크판위에서 2 (49ms, 16MB)

---
+++ 260816/체크판위에서 2/on-the-checkboard-2.py
@@ -0,0 +1,16 @@
+R, C = map(int, input().split())
+grid = [list(input().split()) for _ in range(R)]
+
+ans = 0
+
+for i in range(1, R - 1):
+    for j in range(1, C - 1):
+        for k in range(i + 1, R - 1):
+            for l in range(j + 1, C - 1):
+
+                if grid[0][0] != grid[i][j] \
+                and grid[i][j] != grid[k][l] \
+                and grid[k][l] != grid[R - 1][C - 1]:
+                    ans += 1
+
+print(ans)
