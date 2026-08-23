cae9038698e87dc21f9da222a2c4caf77a2e0f01
hdsjiw <105874382+hdsjiw@users.noreply.github.com>
2026-08-23 23:12:34 +0900

    [Passed] 아름다운 수열 2 (48ms, 16MB)

---
+++ 260823/아름다운 수열 2/beautiful-sequence-2.py
@@ -0,0 +1,16 @@
+N, M = map(int, input().split())
+
+A = list(map(int, input().split()))
+B = list(map(int, input().split()))
+
+B.sort()
+
+cnt = 0
+
+for i in range(N - M + 1):
+    sub = A[i:i + M]
+
+    if sorted(sub) == B:
+        cnt += 1
+
+print(cnt)
